6f5af40ae4c84ecd69cb8ad2d11b17bdd5c3fbfd
Emon <jakariachowdhury2427@gmail.com>
2025-10-31 23:38:15 +0600

    2nd commit

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-for (let i = 0; i < 5; i++) {
-     console.log(`Iteration number: ${i}`);
+
+const timeout = ms => new Promise(res => setTimeout(res, ms));
+
+(async () => {
+    console.log('Starting delay...');
+    await timeout(200000); // 200 seconds
+    console.log('Delay of 200 seconds finished.');
+})();
+
+for (let i = 0; i < 50000; i++) {
+    await timeout(100);
+    console.log(`Iteration number: ${i}`);
 }
